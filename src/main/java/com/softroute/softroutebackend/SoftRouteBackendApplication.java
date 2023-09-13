bdbcf8a3167e351e8f9cd7c50fbb6d41e946f2d8
Rojoxt <delamarzp@gmail.com>
2023-09-12 21:36:11 -0500

    git init

---
+++ src/main/java/com/softroute/softroutebackend/SoftRouteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.softroute.softroutebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftRouteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftRouteBackendApplication.class, args);
+    }
+
+}
