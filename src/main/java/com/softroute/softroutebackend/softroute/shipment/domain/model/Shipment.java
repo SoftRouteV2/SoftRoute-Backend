d5eae76cc9cd8b69fa55f07acfc5367e62d84129
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-10-07 16:29:21 -0500

    (Shipment): add shipment entity

--- src/main/java/com/softroute/softroutebackend/softroute/shipment/domain/model/Shipment.java
+++ src/main/java/com/softroute/softroutebackend/softroute/shipment/domain/model/Shipment.java
@@ -15,8 +15,7 @@ import java.util.Date;
 public class Shipment  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    private Long id;
     private String description;
     private Long code;
     private Double freight;
