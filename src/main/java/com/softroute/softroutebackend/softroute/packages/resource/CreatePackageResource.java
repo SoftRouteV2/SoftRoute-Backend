67ecb8bd4a2ccc6499db6c41b3af882e535f9cf8
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-10-17 22:28:58 -0500

    (Package): added Package create resource

---
+++ src/main/java/com/softroute/softroutebackend/softroute/packages/resource/CreatePackageResource.java
@@ -0,0 +1,26 @@
+package com.softroute.softroutebackend.softroute.packages.resource;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePackageResource {
+    @NotBlank
+    private String description;
+    @NotNull
+    private Long code;
+    @NotNull
+    private Double weight;
+    @NotNull
+    private Double length;
+    @NotNull
+    private Double width;
+    @NotNull
+    private Double height;
+
+}
