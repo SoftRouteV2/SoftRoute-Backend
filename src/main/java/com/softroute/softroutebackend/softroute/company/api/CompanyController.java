83f2560a4246b9dec0640276a0db3943f09d0b5b
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-10-21 17:40:30 -0500

    (Company) company controller added

---
+++ src/main/java/com/softroute/softroutebackend/softroute/company/api/CompanyController.java
@@ -0,0 +1,4 @@
+package com.softroute.softroutebackend.softroute.company.api;
+
+public class CompanyController {
+}
--- src/main/java/com/softroute/softroutebackend/softroute/shipment/api/ShipmentController.java
+++ src/main/java/com/softroute/softroutebackend/softroute/shipment/api/ShipmentController.java
@@ -11,8 +11,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
