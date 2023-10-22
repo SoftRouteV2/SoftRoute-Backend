5e2e0535daaffd48e13ca913111292a9fb61c846
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-10-21 22:01:03 -0500

    (Employee) employee entity added

---
+++ src/main/java/com/softroute/softroutebackend/softroute/employee/domain/model/Employee.java
@@ -0,0 +1,41 @@
+package com.softroute.softroutebackend.softroute.employee.domain.model;
+
+import com.softroute.softroutebackend.softroute.company.domain.model.Company;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+    @NotNull
+    @Column(name = "employee_dni")
+    @Size(max = 8)
+    private Long dni;
+    @NotBlank
+    @Column(name = "employee_name")
+    @Size(max = 100)
+    private String employeeName;
+    @NotBlank
+    @Column(name = "employee_password", nullable = false)
+    @Size(max = 100)
+    private String password;
+    @NotBlank
+    @Column(name = "employee_email", nullable = false)
+    @Size(max = 100)
+    private String email;
+    //relationships
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+}
