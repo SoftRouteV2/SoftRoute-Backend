542392e57f2d2e3f3f46cf208608adde686d5d6f
LenninHuaman <u20201b975@upc.edu.pe>
2023-11-20 23:17:41 -0500

    Implement create test

--- src/test/java/com/softroute/softroutebackend/Dht22ControllerTest.java
+++ src/test/java/com/softroute/softroutebackend/Dht22ControllerTest.java
@@ -1,13 +1,23 @@
 package com.softroute.softroutebackend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.softroute.softroutebackend.softroute.dth22.api.Dht22Controller;
+import com.softroute.softroutebackend.softroute.dth22.domain.model.Dht22;
 import com.softroute.softroutebackend.softroute.dth22.domain.service.Dht22Service;
 import com.softroute.softroutebackend.softroute.dth22.mapping.Dht22Mapper;
+import com.softroute.softroutebackend.softroute.dth22.resource.CreateDht22Resource;
+import com.softroute.softroutebackend.softroute.dth22.resource.Dht22Resource;
 
+@ExtendWith(MockitoExtension.class)
 public class Dht22ControllerTest {
 
     @Mock
@@ -22,7 +32,15 @@ public class Dht22ControllerTest {
 
     @Test
     void testCreateDht22() {
+        // Mock data
+        CreateDht22Resource createDth22Resource = new CreateDht22Resource();
+        Dht22Resource mockDht22 = new Dht22Resource();
+        when(mapper.toModel(createDth22Resource)).thenReturn(new Dht22());
+        when(dht22Service.create(any(Dht22.class))).thenReturn(new Dht22());
+        when(mapper.toResource(any(Dht22.class))).thenReturn(mockDht22);
 
+        Dht22Resource result = dht22Controller.createDht22(createDth22Resource);
+        assertEquals(mockDht22, result);
     }
 
     @Test
